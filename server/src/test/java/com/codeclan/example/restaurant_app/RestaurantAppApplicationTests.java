8517b0db679cca28a3e9f11318d44a0e0f1f21e2
Alex Woods <alex.woods89@hotmail.co.uk>
2019-11-01 09:53:39 +0000

    file structure started

---
+++ server/src/test/java/com/codeclan/example/restaurant_app/RestaurantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.restaurant_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
