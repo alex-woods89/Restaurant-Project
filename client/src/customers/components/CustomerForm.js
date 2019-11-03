5e781aa4d2a5ad2ce6d95396f77352e83f46651a
Cameron Pellett <bubionbreakfast@gmail.com>
2019-11-03 13:39:52 +0000

    customerform feilds added

--- client/src/customers/components/CustomerForm.js
+++ client/src/customers/components/CustomerForm.js
@@ -5,37 +5,66 @@ class CustomerForm extends Component {
         super(props);
         this.state = {
             name: '',
-            phoneNumber: '',
-            email: '',
+            phone: '',
+            email: ''
 
         }
         this.handleNameChange = this.handleNameChange.bind(this);
-        this.handlephoneNumberChange = this.handlephoneNumberChange.bind(this);
+        this.handlePhoneChange = this.handlePhoneChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleNameChange(event){
         this.setState({name: event.target.value})
     }
 
-    handlephoneNumberChange(event){
-        this.setState({phoneNumber: event.target.value})
+    handlePhoneChange(event){
+        this.setState({phone: event.target.value})
     }
 
-    handleemailChange(event){
+    handleEmailChange(event){
         this.setState({email: event.target.value})
     }
 
     handleSubmit(event){
         event.preventDefault();
         const name = this.state.name.trim();
-        const phoneNumber = this.state.phoneNumber.trim();
+        const phone = this.state.phone.trim();
         const email = this.state.email.trim();
 
 
-        this.props.onCustomerSubmit({name: name, phoneNumber: phoneNumber, email: email})
+        this.props.onCustomerSubmit({name: name, phone: phone, email: email})
         //todo: update list of comments
 
-        this.setState({name: '', phoneNumber: '', email: ''});
+        this.setState({name: '', phone: '', email: ''});
+    }
+
+    render(){
+        return(
+            <form className="customer-form" onSubmit={this.handleSubmit}>
+                <input 
+                type="text"
+                placeholder="Your Name"
+                value={this.state.name}
+                onChange={this.handleNameChange}
+                />
+                <input
+                type="text" placeholder="Phone Number"
+                value={this.state.phone}
+                onChange={this.handlePhoneChange}
+                />
+                <input
+                type="text" placeholder="Email"
+                value={this.state.email}
+                onChange={this.handleEmailChange}
+                />
+                <input
+                type="submit"
+                value="Post"
+                />
+
+            </form>
+        )
     }
 }
 
