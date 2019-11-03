41a1bb90007cc531a5e067b5ab3a9302f7668102
Cameron Pellett <bubionbreakfast@gmail.com>
2019-11-03 11:46:59 +0000

    basic 3 route hooks and error page

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,6 +1,10 @@
 import React, { Component, Fragment } from 'react';
 import NavBar from './NavBar';
+import Home from './home/components/Home'
 import Booking from './bookings/components/Booking';
+import Customer from './customers/components/Customer';
+import ErrorPage from './ErrorPage';
+
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 
@@ -20,8 +24,10 @@ class Main extends Component {
                 <Fragment>
                     <NavBar />
                     <Switch>
-                        {/* <Route exact path="/" component={Home}/> */}
+                        <Route exact path="/" component={Home}/>
                         <Route path="/bookings" component={Booking}/>
+                        <Route path="/customers" component={Customer}/>
+                        <Route component={ErrorPage}/>
 
                     </Switch>
                 </Fragment>
