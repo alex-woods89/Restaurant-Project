7e8d74c8cda5f51f1675262534f460ff7fc0b82f
Alex Woods <alex.woods89@hotmail.co.uk>
2019-11-05 09:06:22 +0000

    readded controllers

---
+++ server/src/main/java/com/codeclan/example/restaurant_app/controllers/CustomerController.java
@@ -0,0 +1,15 @@
+package com.codeclan.example.restaurant_app.controllers;
+
+
+import com.codeclan.example.restaurant_app.repositories.CustomerRepository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/customers")
+public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+}
---
+++ server/src/main/java/com/codeclan/example/restaurant_app/controllers/SeatingController.java
@@ -0,0 +1,10 @@
+package com.codeclan.example.restaurant_app.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/seatings")
+public class SeatingController {
+}
