828fdf26a79c0e61ae0f273787e747f114c1cc74
Joe Cooper <josephcooper3@gmail.com>
2019-11-05 09:49:18 +0000

    adds and edits seating seeds

--- server/src/main/java/com/codeclan/example/restaurant_app/components/DataLoader.java
+++ server/src/main/java/com/codeclan/example/restaurant_app/components/DataLoader.java
@@ -35,11 +35,23 @@ public class DataLoader implements ApplicationRunner {
         Customer customer2 = new Customer("Godfreya Richards", "godfreya@example.com", "1098 765 4321");
         customerRepository.save(customer2);
 
-        Seating seating1 = new Seating(1, 57);
+        Seating seating1 = new Seating(2, 1);
         seatingRepository.save(seating1);
 
-        Seating seating2 = new Seating(4, 12);
+        Seating seating2 = new Seating(2, 2);
         seatingRepository.save(seating2);
+
+        Seating seating3 = new Seating(4, 3);
+        seatingRepository.save(seating3);
+
+        Seating seating4 = new Seating(4, 4);
+        seatingRepository.save(seating4);
+
+        Seating seating5 = new Seating(6, 5);
+        seatingRepository.save(seating5);
+
+        Seating seating6 = new Seating(8, 6);
+        seatingRepository.save(seating6);
 
         Booking booking1 = new Booking("2019-11-01", "19:00", 1, "allergic to pulses", customer1, seating1);
         bookingRepository.save(booking1);
