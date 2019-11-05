9dfa25362915164987ba9c495c74864b485a1b3e
Alex Woods <alex.woods89@hotmail.co.uk>
2019-11-05 09:24:15 +0000

    booking selected online

--- client/src/components/bookingcomponents/BookingDetail.js
+++ client/src/components/bookingcomponents/BookingDetail.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react'
+import React from 'react'
 
 const BookingDetail = ({booking}) => {
-
     if(!booking) return null;
     return (
+      
         <div>
             <h2>Booking Details</h2>
             <ul>
+                
                 <li>Date: {booking.date}</li>
                 <li>Time: {booking.time}</li>
                 <li>Customer Name: {booking._embedded.customer.name}</li>
--- client/src/components/bookingcomponents/BookingList.js
+++ client/src/components/bookingcomponents/BookingList.js
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, {Component, Fragment} from 'react'
 
-const BookingList = (props) => {
+class BookingList extends Component {
+         constructor(props){
+           super(props);
+           this.state = {
+             selectedIndex: -1
+           }
+           this.handleChange = this.handleChange.bind(this)
+         }
 
-        const options = props.bookings.map((booking, index) => {
-           return <option value={booking.id} key={index}>{booking.date}  {booking.time}</option>
+          handleChange(event) {
+            this.setState({selectedIndex: event.target.value})
+          this.props.onBookingSelected(event.target.value);
+        }
+         render(){
+        const options = this.props.bookings.map((booking, index) => {
+          console.log(booking)
+           return <option value={index} key={index}>{booking.date}  {booking.time}</option>
         })
 
-        function handleChange(event) {
-            props.onBookingSelected(event.target.value);
-          }
+        
 
     return(
-        <select id="booking-selector" onChange={handleChange} defaultValue="default">
-      <option disabled value="default">Choose a booking...</option>
+      <Fragment>
+        <select id="booking-selector" onChange={this.handleChange} value={this.state.selectedIndex} defaultValue="default">
+      <option disabled value={-1}>Choose a booking...</option>
       {options}
     </select>
+    </Fragment>
     )
-}
+         }
+  }
 
 export default BookingList
