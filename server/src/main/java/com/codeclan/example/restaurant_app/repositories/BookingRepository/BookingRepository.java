d513406826ebfeda9d536959d04a4e03a019d420
cam <bubionbreakfast@gmail.com>
2019-11-05 15:11:37 +0000

    booking by date

--- server/src/main/java/com/codeclan/example/restaurant_app/repositories/BookingRepository/BookingRepository.java
+++ server/src/main/java/com/codeclan/example/restaurant_app/repositories/BookingRepository/BookingRepository.java
@@ -4,6 +4,9 @@ import com.codeclan.example.restaurant_app.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findBookingByDate(String date);
 }
