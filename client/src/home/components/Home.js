b785dc142f865e09eea3c9c70cba1d9127b535d1
Davide Campagna <gdavidecampagna@gmail.com>
2019-11-06 09:21:39 +0000

    working on home button

--- client/src/home/components/Home.js
+++ client/src/home/components/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 const Home = () => (
     <div className="container">
         <h4>Welcome to Godfreya's!</h4>
         <img id="home-pic" src="http://3.bp.blogspot.com/-zF7_8YHOOT4/VSFOSWHwukI/AAAAAAAADeE/wdbDbs6JbRg/s1600/19.jpg" />
+        <Button
+          variant="contained"
+          color="secondary"
+          type="submit">
+          Make a reservation
+        </Button>
     </div>
 )
 
